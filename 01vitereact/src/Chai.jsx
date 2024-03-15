2e7546d37717da17e5921f758d49c661dca55f27
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-03-16 02:39:52 +0530

    commit

---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,12 @@
+
+function Chai() {
+    return (
+        <>
+        <h3>Chai is ready</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adi</p>
+        </>
+        
+    )
+}
+
+export default Chai

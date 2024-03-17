32d2328f474c60f949c5ea077d30b8e00307cab7
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-03-17 22:32:16 +0530

    theme switcher context project

---
+++ 09themeswitcher/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html, js, ts, vue}",
+    "./src/**/*"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

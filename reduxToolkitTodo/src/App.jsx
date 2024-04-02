e279efc2f1a6c0126ff834575c6118b0f9a90ff5
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-04-02 23:29:14 +0530

    reduxToolkitTodo project

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from "./components/Todos";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores corrupti autem culpa!</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores corrupti autem culpa!</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }

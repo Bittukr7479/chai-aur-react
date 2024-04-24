ac13ce928f4215a7f36971a95be777b922776d71
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-04-25 00:31:17 +0530

    Revesion

--- reduxToolkitTodo/tailwind.config.js
+++ reduxToolkitTodo/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{html, js, ts, vue}",
-    "./src/**/*"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {},
   },

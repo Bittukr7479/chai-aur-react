689d8a3387de6dcec254aaa0c9115f4d75e3ec73
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-05-04 02:02:41 +0530

    error solved

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
-    {/* <Header/>
-    <Home/>
-    <Footer/> */}
-    </>
-  )
-}
+//   return (
+//     <>
+//     {/* <Header/>
+//     <Home/>
+//     <Footer/> */}
+//     </>
+//   )
+// }
 
-// export default App
+// // export default App
